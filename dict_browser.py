586dde545ad067735bb53d184e6c573c51f47a23
Bas Nijholt <basnijholt@gmail.com>
2020-03-23 17:06:59 +0100

    unify loops

--- dict_browser.py
+++ dict_browser.py
@@ -35,10 +35,12 @@ def draw_table(nested_keys, table, box, max_nrows=30):
     selected_table = get_in(nested_keys, table)
     nrows = sum(len(v) if _should_expand(v) else 1 for v in selected_table.values()) + 1
     ncols = 3
+
     if nrows > max_nrows:
         nrows = len(selected_table) + 1
         col_widths.pop(1)
         ncols = 2
+
     grid = GridspecLayout(nrows, col_widths[-1])
     update = partial(_update, table=table, box=box)
 
@@ -49,17 +51,18 @@ def draw_table(nested_keys, table, box, max_nrows=30):
     grid[0, -1] = button("↰", "info", up_click)
 
     # Body
-    if ncols == 3:
-        i = 1
-        for k, v in selected_table.items():
-            row_length = len(v) if _should_expand(v) else 1
-            but = button(k, "info", up_click)
-            grid[i : i + row_length, : col_widths[0]] = but
-            if _should_expand(v):
+
+    i = 1
+    for k, v in selected_table.items():
+        row_length = len(v) if _should_expand(v) and ncols == 3 else 1
+        but = button(k, "info", up_click)
+        grid[i : i + row_length, : col_widths[0]] = but
+        if _should_expand(v):
+            if ncols == 3:
                 for k_, v_ in v.items():
                     but = button(k_, "danger", update([*nested_keys, k]))
                     grid[i, col_widths[0] : col_widths[1]] = but
-                    if isinstance(v_, dict) and v_ != {}:
+                    if _should_expand(v_):
                         sub_keys = ", ".join(v_.keys())
                         but = button(sub_keys, "warning", update([*nested_keys, k, k_]))
                     else:
@@ -67,18 +70,14 @@ def draw_table(nested_keys, table, box, max_nrows=30):
                     grid[i, col_widths[1] :] = but
                     i += 1
             else:
-                grid[i, col_widths[0] :] = text(str(v))
-                i += 1
-    else:
-        for i, (k, v) in enumerate(selected_table.items()):
-            grid[i + 1, : col_widths[0]] = button(k, "info", up_click)
-            if _should_expand(v):
                 sub_keys = ", ".join(v.keys())
-                grid[i + 1, col_widths[0] :] = button(
+                grid[i, col_widths[0] :] = button(
                     sub_keys, "danger", update([*nested_keys, k])
                 )
-            else:
-                grid[i + 1, col_widths[0] :] = text(str(v))
+                i += 1
+        else:
+            grid[i, col_widths[0] :] = text(str(v))
+            i += 1
     return grid
 
 
